7e74f75ed86d99b371e7c7a0a2bd3e2549afd75e
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-02-18 03:56:01 +0600

    Create problem 1002 Area of a Circle.cpp

---
+++ problem 1002 Area of a Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n=3.14159, A, R;
+
+    cin >> R;
+    A = n*(R*R);
+    cout << fixed << setprecision(4) << "A=" << A << endl;
+
+    return 0;
+}
