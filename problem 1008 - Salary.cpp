4516781681db676430a4ead576dac646016ee94e
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-02-24 19:15:30 +0600

    Create problem 1008 - Salary.cpp

---
+++ problem 1008 - Salary.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A, NUMBER;
+    double B, SALARY;
+
+    cin >> NUMBER >> A >> B;
+    SALARY = A * B;
+    
+    cout << "NUMBER = " << NUMBER << endl;
+    cout << fixed;
+    cout << setprecision(2) << "SALARY = U$ " << SALARY << endl;
+
+    return 0;
+
+}
