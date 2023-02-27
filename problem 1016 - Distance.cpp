1108e3b0880ba03516d1879bc199e3114f64a094
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-02-27 22:11:05 +0600

    Create problem 1016 - Distance.cpp

---
+++ problem 1016 - Distance.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int distance, minutos;
+    cin >> distance;
+    minutos = distance * 2;
+
+    cout << minutos << " minutos" << endl;
+
+    return 0;
+}
