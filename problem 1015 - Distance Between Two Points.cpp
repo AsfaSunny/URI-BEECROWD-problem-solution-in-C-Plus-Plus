0544e8b4021e4b1e3f9332ffa95a810608b58e65
Ariful Asfake Sunny <92142951+AsfaSunny@users.noreply.github.com>
2023-02-27 22:04:31 +0600

    Create problem 1015 - Distance Between Two Points.cpp

---
+++ problem 1015 - Distance Between Two Points.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main() {
+    double x1, y1, x2, y2, Distance;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    
+    Distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    cout << fixed;
+    cout << setprecision(4) << Distance << endl;
+    
+    return 0;
+}
